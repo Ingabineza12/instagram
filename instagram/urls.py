6ba6163f91059ddd791ba4f977bcce0a04143622
Deborah M Ingabire <ingabineza@gmail.com>
2021-01-05 20:43:09 +0200

    added urls in instagram/urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,18 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.home_images,name = 'homePage'),
+    url(r'^new/image$',views.new_image,name = 'new_image'),
+    url(r'^edit/profile$',  views.profile_edit,name='profile_edit'),
+    url(r'^myprofile/$',  views.profilemy,name='profilemy'),
+    url(r'^comment/(\d+)/$', views.add_comment, name='comment'),
+    url(r'^likes/(?P<id>\d+)',views.likes,name="like"),
+    url(r'^search/',  views.search_results,name='search_results'),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
